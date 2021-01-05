3094d77159f012de493f3988581bc5b47aa46507
lizza <liutao_glory@126.com>
2021-01-05 16:27:44 +0800

    lizza commit.

---
+++ distributed-lock/src/main/java/com/lizza/distributed_lock/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.lizza.distributed_lock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfig {
+
+    public RedisTemplate<String, Serializable> redisTemplate (LettuceConnectionFactory factory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(factory);
+
+        return template;
+    }
+}
